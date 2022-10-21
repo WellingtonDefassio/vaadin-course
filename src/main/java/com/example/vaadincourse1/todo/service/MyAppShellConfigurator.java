2d63e22481ddfca3db4a9b314a7c769119373802
Wellington Defassio <well.defassio@gmail.com>
2022-10-20 21:52:53 -0300

    feat: vaadin excel.

---
+++ src/main/java/com/example/vaadincourse1/todo/service/MyAppShellConfigurator.java
@@ -0,0 +1,10 @@
+package com.example.vaadincourse1.todo.service;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+
+@Push
+public class MyAppShellConfigurator implements AppShellConfigurator {
+
+
+}
